9075f8a3a4918ef4f79fb1b253ef8e88b729385c
holbertc <holbert.christopher@gmail.com>
2017-02-17 11:23:51 -0500

    Add files via upload

--- baseline.py
+++ baseline.py
@@ -17,7 +17,7 @@ from keras.utils import np_utils
 
 batch_size = 128
 nb_classes = 10
-nb_epoch = 25
+nb_epoch = 35
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -50,7 +50,7 @@ model.add(Activation('softmax'))
 
 model.summary()
 
-sgd=SGD(lr=0.1)
+sgd=SGD(lr=0.1, decay=0.0)
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd,
               metrics=['accuracy'])
