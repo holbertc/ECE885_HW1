08c9ce5c23ef0b8ed6ba86f87dfc6df2fe568f8d
holbertc <holbert.christopher@gmail.com>
2017-02-17 11:43:05 -0500

    Add files via upload

--- baseline.py
+++ baseline.py
@@ -17,7 +17,7 @@ from keras.utils import np_utils
 
 batch_size = 128
 nb_classes = 10
-nb_epoch = 10
+nb_epoch = 3
 
 # the data, shuffled and split between train and test sets
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
